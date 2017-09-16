6d1b66bf3092fa19ff08642d67ec03b965107b2f
Robert Adkins <robertadkins00@gmail.com>
2017-09-16 17:47:31 -0400

    implemented adding nodes / tracks to the model, testing with the synth

--- model.js
+++ model.js
@@ -2,19 +2,23 @@
 
 
 function Model() {
-    this.tracks = [];
+  this.beat = 0; // beats are 16th notes for now. so tempo / 4 is a beat.
+  this.playInterval = null;
+  this.tempo = 120.0; // bpm
+  this.tracks = [];
+  this.trackLength = 16; // steps
 }
 
-Model.prototype.addNote = function(track, beat, pitch, noteData) {
-    tracks[track].addNote(beat, pitch, noteData);
+Model.prototype.addNote = function(track, beat, noteData) {
+    this.tracks[track].addNote(beat, noteData);
 };
 
 Model.prototype.deleteNote = function(track, beat, pitch) {
     tracks[track].deleteNote(beat, pitch);
 };
 
-Model.prototype.addTrack = function(index) {
-    
+Model.prototype.addTrack = function(instrument, index) {
+  this.tracks[index] = new Track(new TrackData(instrument, this.tempo));
 };
 
 Model.prototype.modifyTrack = function(index, trackData) {
@@ -29,26 +33,107 @@ Model.prototype.projectSettings = function(settings) {
 
 };
 
+Model.prototype.pause = function() {
+  if (this.playInterval) {
+    clearInterval(this.playInterval);
+    this.playInterval = null;
+  }
+};
+
+Model.prototype.play = function() {
+  if (this.playInterval) {
+    return;
+  }
+  // TODO: probably not a good way to do this. Should keep time using current
+  // time to minimize time drift
+  this.playInterval = setInterval(
+    function() {
+      for (let i = 0; i < this.tracks.length; i++) {
+	this.tracks[i].play(this.beat);
+      }
+      this.beat = (this.beat + 1) % this.trackLength;
+    }.bind(this),
+    1.0 / this.tempo / 4.0 * 60.0 * 1000.0,
+  );
+};
+
+Model.prototype.stop = function() {
+  this.pause();
+  this.beat = 0;
+};
+
 
 
 function Track(trackData) {
     this.trackData = trackData;
     this.notes = [];
-}
+};
 
-Track.prototype.addNote = function(beat, pitch, noteData) {
-    var n = new Note(noteData);
-    if (!notes[beat]) {
-        notes[beat] = [];
+Track.prototype.addNote = function(beat, notedata) {
+    var n = new Note(notedata);
+    if (!this.notes[beat]) {
+        this.notes[beat] = [];
     }
-    notes[beat].append(n);
+    this.notes[beat].push(n);
 };
 
 Track.prototype.deleteNote = function(beat, pitch) {
 
 };
 
+Track.prototype.play = function(beat) {
+  if (!this.notes[beat]) {
+    return;
+  }
+  for (let i = 0; i < this.notes[beat].length; i++) {
+    console.log(this.notes[beat][i]);
+    console.log(this.notes[beat][i].noteData.pitch);
+    console.log(this.notes[beat][i].noteData.duration / 4.0 / this.trackData.tempo * 60.0);
+    this.trackData.instrument.play(
+      this.notes[beat][i].noteData.pitch,
+      this.notes[beat][i].noteData.duration / 4.0 / this.trackData.tempo * 60.0, // convert to secs
+    ); // Assume this is common interface among instruments
+  }
+}
+
+
+function TrackData(instrument, tempo) {
+  this.instrument = instrument;
+  this.tempo = tempo;
+};
+
+
 
 function Note(noteData) {
     this.noteData = noteData;
+};
+
+  
+
+function NoteData(pitch, duration) {
+  this.pitch = pitch; // MIDI value
+  this.duration = duration; // in "beats"
+};
+
+
+// Test functions
+function testModel() {
+  let model = new Model();
+  let synth = new Synth('square', 'square');
+  synth.offset = 1200;
+  model.addTrack(synth, 0);
+  
+  model.addNote(0, 0, new NoteData(60, 2));
+  model.addNote(0, 0, new NoteData(64, 2));
+  
+  model.addNote(0, 4, new NoteData(64, 2));
+  model.addNote(0, 4, new NoteData(67, 2));
+  
+  model.addNote(0, 8, new NoteData(67, 4));
+  model.addNote(0, 8, new NoteData(71, 4));
+  
+  model.addNote(0, 12, new NoteData(64, 2));
+  model.addNote(0, 12, new NoteData(67, 2));
+
+  model.play();
 }
