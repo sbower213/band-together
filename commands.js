5961bf356d40012eed23a1895ac688145956a119
Jonah Chazan <person.1123.loztp@gmail.com>
2017-09-16 15:20:13 -0400

    Completed OT table (I think)

--- commands.js
+++ commands.js
@@ -19,6 +19,129 @@ for (var key in commandTypes.keys()) {
     transformTable[commandTypes[key]] = {};
 }
 
+transformTable[commandTypes.ADD_TRACK][commandTypes.ADD_TRACK] =
+    function(c1, c2) {
+        if (c1.index < c2.index ||
+            c1.index == c2.index && c1.sessionIndex < c2.sessionIndex) {
+            return new AddTrack(c2.index + 1, c2.sessionIndex);
+        }
+        return c2;
+    };
+transformTable[commandTypes.ADD_TRACK][commandTypes.MODIFY_TRACK] =
+    function(c1, c2) {
+        if (c1.index <= c2.index) {
+            return new ModifyTrack(c2.index + 1, c2.trackData, c2.sessionIndex);
+        }
+        return c2;
+    };
+transformTable[commandTypes.ADD_TRACK][commandTypes.DELETE_TRACK] =
+    function(c1, c2) {
+        if (c1.index <= c2.index) {
+            return new DeleteTrack(c2.index + 1, c2.sessionIndex);
+        }
+        return c2;
+    };
+transformTable[commandTypes.ADD_TRACK][commandTypes.INSERT_NOTE] =
+    function(c1, c2) {
+        if (c1.index <= c2.index) {
+            return new InsertNote(c2.index + 1, c2.beat, c2.pitch, c2.noteData, c2.sessionIndex);
+        }
+        return c2;
+    };
+transformTable[commandTypes.ADD_TRACK][commandTypes.MODIFY_NOTE] =
+    function(c1, c2) {
+        if (c1.index <= c2.index) {
+            return new ModifyNote(c2.index + 1, c2.beat, c2.pitch, c2.noteData, c2.sessionIndex);
+        }
+        return c2;
+    };
+transformTable[commandTypes.ADD_TRACK][commandTypes.DELETE_NOTE] =
+    function(c1, c2) {
+        if (c1.index <= c2.index) {
+            return new DeleteNote(c2.index + 1, c2.beat, c2.pitch, c2.sessionIndex);
+        }
+    };
+
+
+
+transformTable[commandTypes.DELETE_TRACK][commandTypes.ADD_TRACK] =
+    function(c1, c2) {
+        if (c1.index < c2.index) {
+            return new AddTrack(c2.index - 1, c2.sessionIndex);
+        }
+        return c2;
+    };
+transformTable[commandTypes.DELETE_TRACK][commandTypes.MODIFY_TRACK] =
+    function(c1, c2) {
+        if (c1.index < c2.index) {
+            return new DeleteTrack(c2.index - 1, c2.sessionIndex);
+        }
+        return c2;
+    };
+transformTable[commandTypes.DELETE_TRACK][commandTypes.DELETE_TRACK] =
+    function(c1, c2) {
+        if (c1.index < c2.index) {
+            return new DeleteTrack(c2.index - 1, c2.sessionIndex);
+        }
+        if (c1.index == c2.index) {
+            return new Nop(c2.sessionIndex);
+        }
+        return c2;
+    };
+transformTable[commandTypes.DELETE_TRACK][commandTypes.INSERT_NOTE] =
+    function(c1, c2) {
+        if (c1.index < c2.index) {
+            return new InsertNote(c2.index - 1, c2.beat, c2.pitch, c2.noteData, c2.sessionIndex);
+        }
+        if (c1.index == c2.index) {
+            return new Nop(c2.sessionIndex);
+        }
+        return c2;
+    };
+transformTable[commandTypes.DELETE_TRACK][commandTypes.MODIFY_NOTE] =
+    function(c1, c2) {
+        if (c1.index < c2.index) {
+            return new ModifyNote(c2.index - 1, c2.beat, c2.pitch, c2.noteData, c2.sessionIndex);
+        }
+        if (c1.index == c2.index) {
+            return new Nop(c2.sessionIndex);
+        }
+        return c2;
+    };
+transformTable[commandTypes.DELETE_TRACK][commandTypes.DELETE_NOTE] =
+    function(c1, c2) {
+        if (c1.index < c2.index) {
+            return new DeleteNote(c2.index - 1, c2.beat, c2.pitch, c2.sessionIndex);
+        }
+        if (c1.index == c2.index) {
+            return new Nop(c2.sessionIndex);
+        }
+        return c2;
+    };
+
+
+transformTable[commandTypes.INSERT_NOTE][commandTypes.INSERT_NOTE] =
+    function(c1, c2) {
+        if (c1.index == c2.index && c1.beat == c2.beat && c1.pitch == c2.pitch &&
+           c1.sessionIndex < c2.sessionIndex) {
+            return new Nop(c2.sessionIndex);
+        }
+        return c2;
+    };
+transformTable[commandTypes.INSERT_NOTE][commandTypes.MODIFY_NOTE] =
+    function(c1, c2) {
+        if (c1.index == c2.index && c1.beat == c2.beat && c1.pitch == c2.pitch) {
+            return new Nop(c2.sessionIndex);
+        }
+        return c2;
+    };
+transformTable[commandTypes.INSERT_NOTE][commandTypes.DELETE_NOTE] =
+    function(c1, c2) {
+        if (c1.index == c2.index && c1.beat == c2.beat && c1.pitch == c2.pitch) {
+            return new Nop(c2.sessionIndex);
+        }
+        return c2;
+    };
 
 
 // ABSTRACT CLASS!
@@ -27,7 +150,12 @@ function Command() {
 };
 
 Command.prototype.transform = function(c2) {
-    return transformTable[this.type][c2.type](this, c2);
+    var transform = transformTable[this.type][c2.type];
+
+    if (transform) {
+        return transform(this, c2);
+    }
+    return c2;
 }
 
 Command.prototype.resolve = function(model) {
@@ -36,9 +164,9 @@ Command.prototype.resolve = function(model) {
 
 
 
-function AddTrack(offset) {
+function AddTrack(index) {
     this.type = commandTypes.ADD_TRACK
-    this.offset = offset;
+    this.index = index;
 }
 AddTrack.prototype = Command.prototype;
 
