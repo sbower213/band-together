2551de52bf9bc8c1f7c93a35458f0f9ba6a713ad
Steven Bower <sbower@terpmail.umd.edu>
2017-09-17 03:42:57 -0400

    hey jonah try using real methods next time :p

--- components/track.js
+++ components/track.js
@@ -3,18 +3,18 @@
 function initTrack(track) {
     track.on('click', function(e) {
         if (!$(".track.expanded").is(track)) {
-	    expandedTrack = track;
+	        expandedTrack = track;
             $(".note").draggable('disable');
             $(".track.expanded .note").each(function(index) {
-                $(this).position({left:$(this).position().left, top:.5 * $(this).position().top});
+                $(this).css({left:$(this).position().left, top: $(this).position().top * 0.5});
             });
             //        $(".track.expanded").css("transform", "");
             $(".track").removeClass('expanded');
             track.addClass('expanded');
-            
+
             $(".trackData.expanded").css("transform", "scale(1, 2)");
             $(".track.expanded .note").each(function(index) {
-                $(this).position({left:$(this).position().left, top:2 * $(this).position().top});
+                $(this).css({left:$(this).position().left, top:2 * $(this).position().top});
             });
             track.find(".note").draggable('enable');
             //open instrument container if one doesn't exist
@@ -32,9 +32,9 @@ function initTrack(track) {
             }
         } else {
             var mouseX = e.pageX - track.offset().left;
-            var mouseY = e.pageY - track.offset().top;
+            var mouseY = e.pageY - track.offset().top * .5;
             var beat = Math.floor(mouseX / 40);
-            var pitch = Math.floor(mouseY / (track.height() / 12.0)) + 60;
+            var pitch = Math.ceil(mouseY / (track.height() / 12.0)) + 60;
             commandProcessor.fire(new InsertNote(track.attr("id"),
                                                  beat,
                                                  pitch,
@@ -42,7 +42,7 @@ function initTrack(track) {
                                                      pitch: pitch,
                                                      duration: 1
                                                  }));
-                                                     
+
         }
     });
 }
