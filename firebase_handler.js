669e74e68641a442080d539dfc255df879a6c30d
sidtuli <siddytuli11@gmail.com>
2017-09-16 23:19:11 -0400

    Attempting to handle recieve commands for Firebase

--- firebase_handler.js
+++ firebase_handler.js
@@ -11,7 +11,7 @@ firebase.initializeApp(config);
 var provider = new firebase.auth.GoogleAuthProvider();
 var database = firebase.database();
 var storage = firebase.storage();
-var historyID = "hack"
+var historyID = "hack";
 
 function FireBaseHandler() {
 }
@@ -20,8 +20,22 @@ FireBaseHandler.updateHistory = function(command_index,command) {
     history_entry = {};
     
     console.log(command);
-    history_entry[command_index.toString()+command.sessionId.toString()] = command;
+    history_entry[command_index.toString()+"-"+command.sessionId.toString()] = command;
     
     database.ref("history/"+historyID).update(history_entry);
+
     
 }
+
+// Call Command Processor recieve commands function on change up of data
+database.ref("history/"+historyID).on('child_changed', function(data){
+    command_data = {}
+    command_data.serverCommandCount = data.numChildren();
+    command_data.commands = []
+    data.forEach(function(elt){
+        command_data.commands.push(elt);
+    })
+    CommandProcessor.receiveCommands(command_data);
+    
+    //CommandProcessor.receiveCommands()
+});
